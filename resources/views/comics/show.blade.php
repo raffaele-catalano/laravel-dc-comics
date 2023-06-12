1fbc6e0e239711e2ef83009684d1113d9794f049
raffaele-catalano <raf.catalano@gmail.com>
2023-06-12 21:16:20 +0200

    detailed comic graphic optimization

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,18 +1,18 @@
 @extends('layout.main')
 
 @section('content')
-<h2 class="text-center fw-bold text-warning">{{ $comic->title }}</h2>
+<h2 class="text-center fw-bold text-warning my-3">{{ $comic->title }}</h2>
     <div class="row row-cols-2 w-75 d-flex justify-content-center align-items-center">
-        <div class="col d-flex align-items-center flex-column">
+        <div class="col d-flex align-items-end flex-column">
             <img src="{{ $comic->thumb }}" class="w-50 mb-2" alt="comic-thumb">
-            <p>{{ $comic->description }}</p>
         </div>
         <div class="col">
-            <h4><span class="text-primary">Series:</span> {{ $comic->series }}</h4>
+            <h5><span class="text-primary">Series:</span> {{ $comic->series }}</h5>
             <h5><span class="text-primary">Type:</span> {{ $comic->type }}</h5>
             <h5><span class="text-primary">Sale Date:</span> {{ $comic->sale_date }}</h5>
-            <h5><span class="text-primary">Writers:</span> {{ $comic->writers }}</h5>
-            <h5><span class="text-primary">Artists:</span> {{ $comic->artists }}</h5>
+            <h6><span class="text-primary">Writers:</span> {{ $comic->writers }}</h6>
+            <h6><span class="text-primary">Artists:</span> {{ $comic->artists }}</h6>
+            <p>{{ $comic->description }}</p>
             <h5 class="text-center"><span class="text-primary">Price:</span> {{ $comic->price }}</h5>
         </div>
 
