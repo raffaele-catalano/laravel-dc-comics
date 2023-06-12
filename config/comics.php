cf913d1b58a98d020ace0b7d119bddb20793025c
raffaele-catalano <raf.catalano@gmail.com>
2023-06-12 16:07:50 +0200

    imported db-content

---
+++ config/comics.php
@@ -0,0 +1,215 @@
+<?php
+
+return [
+    [
+        "title" => "Action Comics #1000: The Deluxe Edition",
+        "description" => "The celebration of 1,000 issues of Action Comics continues with a new, Deluxe Edition of the amazing comic that won raves when it hit comics shops in April! This hardcover includes all the stories from that issue, plus the tale by writer Paul Levitz and artist Neal Adams that appeared in the Action Comics: 80 Years Of Superman hardcover, as well as all the variant covers, design sketches by Jim Lee for Superman’s new look, scripts for the stories, the original art from the lost story featuring art by Curt Swan and more! Plus: a complete reprint of the stories that started it all—the Superman stories Action Comics #1 and 2 from 1938!",
+        "thumb" => "https://imgs.search.brave.com/aUNyvZBXUulb963JH7KnQm9AMr8bcBoLsiHREOqayIU/rs:fit:612:612:1/g:ce/aHR0cHM6Ly9pNS53/YWxtYXJ0aW1hZ2Vz/LmNvbS9hc3IvOWZm/ZWYzMDMtMGZhYy00/OGRkLTg3ODctYzUy/NTk0MDk2ODAwXzEu/MTc1ZDk1Mjg2NzY0/OGEwOTczMTY2NGMy/MTE1NjNlYWIuanBl/Zz9vZG5XaWR0aD02/MTImb2RuSGVpZ2h0/PTYxMiZvZG5CZz1m/ZmZmZmY",
+        "price" => "$19.99",
+        "series" => "Action Comics",
+        "sale_date" => "2018-10-02",
+        "type" => "comic book",
+        "artists" => [
+            "José Luis García-López",
+            "Clay Mann",
+            "Rafael Albuquerque",
+            "Patrick Gleason",
+            "Dan Jurgens",
+            "Joe Shuster",
+            "Neal Adams",
+            "Curt Swan",
+            "John Cassaday",
+            "Olivier Coipel",
+            "Jim Lee"
+        ],
+        "writers" => [
+            "Brad Meltzer",
+            "Tom King",
+            "Scott Snyder",
+            "Geoff Johns",
+            "Brian Michael Bendis",
+            "Paul Dini",
+            "Louise Simonson",
+            "Richard Donner",
+            "Marv Wolfman",
+            "Peter J. Tomasi",
+            "Dan Jurgens",
+            "Jerry Siegel",
+            "Paul Levitz"
+        ],
+    ],
+    [
+        "title" => "American Vampire 1976 #1",
+        "description" => "America is broken. Trust between the government and the American public has crumbled. Paranoia reigns supreme. It’s 1976, and this is the concluding chapter of the Eisner Award-winning American Vampire! Skinner Sweet has exhausted all efforts to regain his lost immortality. With his powers and purpose gone, he is now determined to go out with a bang. At a seedy motorcycle rally in the desert where he’s closer than ever to his death wish, Pearl Jones and a shocking partner track him down for one last, desperate mission: The force known as the Gray Trader and its minions are tunneling through the bowels of the world to unleash hell on Earth—just in time for America’s bicentennial. With catastrophe looming, it’s up to Skinner and Pearl to reconcile and change the course of history—or die trying. The series that launched the careers of superstars Scott Snyder and Rafael Albuquerque returns for nine final issues and the closing chapter of the legacy of American Vampire.",
+        "thumb" => "https://imgs.search.brave.com/OU2E3_3vA7flyv-SJ3TcFW1d99KDlp5ZfmW9ylLLFDo/rs:fit:1012:1200:1/g:ce/aHR0cHM6Ly9pMC53/cC5jb20vd3d3LnN1/cGVycG91dm9pci5j/b20vd3AtY29udGVu/dC91cGxvYWRzLzIw/MjAvMDcvQW1lcmlj/YW4tVmFtcGlyZS0x/OTc2LTAxLWNvdi1B/bGJ1cXVlcnF1ZS0y/MDIwLmpwZz9yZXNp/emU9MTAxMiUyQzE1/NTYmc3NsPTE",
+        "price" => "$3.99",
+        "series" => "American Vampire 1976",
+        "sale_date" => "2020-10-06",
+        "type" => "comic book",
+        "artists" => [
+            "Rafael Albuquerque"
+        ],
+        "writers" => [
+            "Scott Snyder"
+        ],
+    ],
+    [
+        "title" => "Aquaman Vol. 4: Underworld",
+        "description" => "Enraged by rumors of Arthur’s survival in the slums of Atlantis, the ruthless King Rath orders the use of ancient Atlantean techno-magic to track down the Aquaman at all costs! But the ex-king Arthur can’t hide for long when his fate collides with that of a mysterious young woman on the run from Rath’s own secret police. Her name: Dolphin. Collects AQUAMAN #25-30.  ",
+        "thumb" => "https://imgs.search.brave.com/gAZQuZyt6WjcUWF6aXCIBaiJqtOJAK-X4nxS9Vbvwno/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9jZG4y/LnBlbmd1aW4uY29t/LmF1L2NvdmVycy9v/cmlnaW5hbC85Nzgx/NDAxMjc1NDI2Lmpw/Zw",
+        "price" => "$16.99",
+        "series" => "Aquaman",
+        "sale_date" => "2018-01-24",
+        "type" => "graphic novel",
+        "artists" => [
+            "Stjepan Sejic"
+        ],
+        "writers" => [
+            "Dan Abnett"
+        ],
+    ],
+    [
+        "title" => "Batgirl #1",
+        "description" => "“Beyond Burnside” Chapter One: The Batgirl you know and love is going global with Eisner Award-winning and New York Times best-selling writer Hope Larson (A Wrinkle in Time, Goldie Vance) and all-star artist Rafael Albuquerque (AMERICAN VAMPIRE). In order to up her game, Babs travels to Japan on a quest to train with the most elite modern combat masters of the East. But when a chance meeting with an old friend puts a target on her back, Batgirl may need to use her new skills to solve a deadly mystery.",
+        "thumb" => "https://imgs.search.brave.com/YET9wY29Aa29TyHqlbieS9qsz3HWyNYQx-WkRqdBRX4/rs:fit:640:984:1/g:ce/aHR0cDovL2dldHlv/dXJjb21pY29uLmNv/LnVrL3dwLWNvbnRl/bnQvdXBsb2Fkcy8y/MDE2LzA4L2JhdGdp/cmwtcmViaXJ0aC0x/LWNvdmVyLmpwZw",
+        "price" => "$2.99",
+        "series" => "Batgirl",
+        "sale_date" => "2016-07-27",
+        "type" => "comic book",
+        "artists" => [
+            "Rafael Albuquerque"
+        ],
+        "writers" => [
+            "Hope Larson"
+        ],
+    ],
+    [
+        "title" => "Batman #56",
+        "description" => "The Dark Knight’s looking to drop both the hammer and sickle on the KGBeast, whose rampage across Gotham City takes a toll on Nightwing when he’s injured in the fray. Blaming himself for his ward’s fate, Batman gets grimmer than usual—and vows to take the Russian assassin down like the Berlin Wall. Is even Gotham City ready for that much violence? The streets will run red like borscht if the Dark Knight gets his way against this Soviet scourge.",
+        "thumb" => "https://imgs.search.brave.com/jgxYlrx442aozemzjqdmCsd9DkIkRollJp1T8sG8TfE/rs:fit:720:1106:1/g:ce/aHR0cHM6Ly9veXN0/ZXIuaWduaW1ncy5j/b20vd29yZHByZXNz/L3N0Zy5pZ24uY29t/LzIwMTgvMTAvU1RM/MDk3MDk1LTcyMHgx/MTA2LmpwZw",
+        "price" => "$3.99",
+        "series" => "Batman",
+        "sale_date" => "2018-10-03",
+        "type" => "comic book",
+        "artists" => [
+            "Tony S. Daniel"
+        ],
+        "writers" => [
+            "Tom King"
+        ],
+    ],
+    [
+        "title" => "Batman Beyond #1",
+        "description" => "“THE RETURN” part one! Terry McGinnis is back as Batman, much to the delight of his family and friends. But is the original Joker really back as well? Inspired by the possible return of their role model, the Jokerz have taken over an entire section of Gotham City and are determined to wreak havoc in the still-rebuilding city. While Batman battles chaos on the streets, his friend Dana is at the mercy of the new leader of the now-unified gang.",
+        "thumb" => "https://imgs.search.brave.com/jgxYlrx442aozemzjqdmCsd9DkIkRollJp1T8sG8TfE/rs:fit:720:1106:1/g:ce/aHR0cHM6Ly9veXN0/ZXIuaWduaW1ncy5j/b20vd29yZHByZXNz/L3N0Zy5pZ24uY29t/LzIwMTgvMTAvU1RM/MDk3MDk1LTcyMHgx/MTA2LmpwZw",
+        "price" => "$2.99",
+        "series" => "Batman Beyond",
+        "sale_date" => "2016-10-26",
+        "type" => "comic book",
+        "artists" => [
+            "Bernard Chang"
+        ],
+        "writers" => [
+            "Dan Jurgens"
+        ],
+    ],
+    [
+        "title" => "Batman/Superman #1",
+        "description" => "Up in the sky, in the dark of the night, trust no one - for the infected walk among us. Spinning out of the devastating events of THE BATMAN WHO LAUGHS, Superman and Batman are together once more in an all-new monthly series - and they're facing a terrifying new threat that could strike from anywhere. The Dark Knight and the Man of Steel must journey into the depths of Gotham City to learn which of their fellow heroes has been transformed into the horrifying horseman of their most dangerous and deranged foe ever. Our heroes will need to fight to survive, but an even more dangerous question lurkes in the shadows: Can Superman and Batman even trust each other?",
+        "thumb" => "https://imgs.search.brave.com/cSaB3m-tghN3_Cllp9IOlUPEPsyShCnl8gijRyPFdrg/rs:fit:791:1200:1/g:ce/aHR0cHM6Ly8xNDk0/NTUxNTIudjIucHJl/c3NhYmxlY2RuLmNv/bS93cC1jb250ZW50/L3VwbG9hZHMvMjAx/OS8wOC9CTVNNX0N2/MV9CQVQuanBn",
+        "price" => "$3.99",
+        "series" => "Batman/Superman",
+        "sale_date" => "2019-08-28",
+        "type" => "comic book",
+        "artists" => [
+            "David Marquez"
+        ],
+        "writers" => [
+            "Joshua Williamson"
+        ],
+    ],
+    [
+        "title" => "Batman/Superman Annual #1",
+        "description" => "On a dark and stormy night in the fifth dimension, two mortal foes meet to settle an age-old question once and for all: In a fight between Batman and Superman, who would win? The combatants? Mr. Mxyzptlk and Bat-Mite. And in this battle for the ages, you will find out if a fifth-dimensional imp can bleed. It’s all in this, the ultimate slugfest between the Dark Knight and the Man of Steel-plus a whole lot of magic!",
+        "thumb" => "https://imgs.search.brave.com/jfaQd--WUx7RG8MiQ-VyCOqLUJW19ojBnjkNOG81rL4/rs:fit:666:1024:1/g:ce/aHR0cHM6Ly90aGVn/d3cuY29tL3dwLWNv/bnRlbnQvdXBsb2Fk/cy8yMDIwLzA5L0Jh/dG1hbi1TdXBlcm1h/bi1Bbm51YWwtQ3Yx/LTY2NngxMDI0Lmpw/Zw",
+        "price" => "$4.99",
+        "series" => "Batman/Superman Annual",
+        "sale_date" => "2020-09-29",
+        "type" => "comic book",
+        "artists" => [
+            "Clayton Henry",
+            "Gleb Melnikov",
+            "Dale Eaglesham"
+        ],
+        "writers" => [
+            "Joshua Williamson"
+        ],
+    ],
+    [
+        "title" => "Batman: The Joker War Zone #1",
+        "description" => "Gotham City is a battleground as The Joker takes over the Wayne fortune and wages a street war against the Dark Knight and his allies! Enter the “war zone” with short stories featuring characters like Cassandra Cain, Stephanie Brown, and Luke Fox and see how they’re fighting back in a city under siege! Also, the brutal full debut of the mysterious new anti-hero known as Clownhunter!",
+        "thumb" => "https://imgs.search.brave.com/kSLCPBIeE9s218LRC4cIyrHOYmxrSS2Wt7LvNNfED2I/rs:fit:455:700:1/g:ce/aHR0cHM6Ly9jb21p/Y2Jvb2tyZWFsbS5j/b20vY292ZXItc2Nh/bi81ZmFmMTFjYTk1/NGQ1MGVjZDA1ZmU3/M2U1NjBiNWQyOC94/bC8tLWlzc3VlLTEu/anBn",
+        "price" => "$5.99",
+        "series" => "Batman: The Joker War Zone",
+        "sale_date" => "2020-09-29",
+        "type" => "comic book",
+        "artists" => [
+            "Guillem March",
+            "Ashley A. Woods",
+            "Olivier Coipel",
+            "James Stokoe",
+            "Darko Lafuente"
+        ],
+        "writers" => [
+            "Sam Johns",
+            "Joshua Williamson",
+            "John Ridley"
+        ],
+    ],
+    [
+        "title" => "Batman: Three Jokers #1",
+        "description" => "Thirty years after Batman: The Killing Joke changed comics forever, Three Jokers reexamines the myth of who, or what, The Joker is and what is at the heart of his eternal battle with Batman. New York Times bestselling writer Geoff Johns and Jason Fabok, the writer/artist team that waged the “Darkseid War” in the pages of Justice League, reunite to tell the ultimate story of Batman and The Joker! After years of anticipation starting in DC Universe: Rebirth #1, the epic miniseries you’ve been waiting for is here: find out why there are three Jokers, and what that means for the Dark Knight and the Clown Prince of Crime. It’s a mystery unlike any Batman has ever faced!",
+        "thumb" => "https://imgs.search.brave.com/a6fAPUra4t94xMJFlYv6X7Lwm4z_qPmslx7Hiqafbok/rs:fit:600:923:1/g:ce/aHR0cHM6Ly9jZG4u/ZmxpY2tlcmluZ215/dGguY29tL3dwLWNv/bnRlbnQvdXBsb2Fk/cy8yMDIwLzA4L0Jh/dG1hbi1UaHJlZS1K/b2tlcnMtMS02MDB4/OTIzLTEuanBn",
+        "price" => "$6.99",
+        "series" => "Batman: Three Jokers",
+        "sale_date" => "2020-08-25",
+        "type" => "comic book",
+        "artists" => [
+            "Jason Fabok"
+        ],
+        "writers" => [
+            "Geoff Johns"
+        ],
+    ],
+    [
+        "title" => "Batman: White Knight Presents: Harley Quinn #1",
+        "description" => "The Joker is dead, Bruce Wayne is behind bars...and Gotham City is just starting to redefine itself without Batman. As Harley Quinn struggles to adjust to her new life as the mother of Jack Napier’s twins, an elusive mastermind called the Producer seizes the moment to assemble a crew of villains-starting with the Starlet, a serial killer who murders Gotham’s golden age film stars in homage to their silver screen roles. When a recent, gruesome crime scene suggests a connection to The Joker, the GTO, and FBI agent Hector Quimby turn to Harley as the one person with information that could crack the case. With some help from Bruce, Harley agrees to investigate-but to protect her children and her city from a fatal final act, Harley must flirt with madness and confront her own past.",
+        "thumb" => "https://imgs.search.brave.com/qMxyCwK99aguaETp4ioxXLrpxwNg2JNBN3Oq7LGAgzM/rs:fit:780:1200:1/g:ce/aHR0cHM6Ly9jZG4u/ZmxpY2tlcmluZ215/dGguY29tL3dwLWNv/bnRlbnQvdXBsb2Fk/cy8yMDIwLzEwL0Jh/dG1hbi1XaGl0ZS1L/bmlnaHQtUHJlc2Vu/dHMtSGFybGV5LVF1/aW5uLTEtMS5qcGc",
+        "price" => "$4.99",
+        "series" => "Batman: White Knight Presents: Harley Quinn",
+        "sale_date" => "2020-10-20",
+        "type" => "comic book",
+        "artists" => [
+            "Matteo Scalera"
+        ],
+        "writers" => [
+            "Katana Collins"
+        ],
+    ],
+    [
+        "title" => "Catwoman Vol. 1: Copycats",
+        "description" => "Following her near-miss wedding with Batman, Catwoman hits the streets to expose a copycat who’s pulling heists around Gotham City. As Selina cracks the whip on her former criminal cohorts, she’s attracting unwanted attention from one of Gotham’s most dangerous groups. The mob? Nope. Try the GCPD. And as if the Bat-Bride didn’t have enough problems, don’t miss the debut of an all-new villain determined to make trouble for all nine of Selina’s lives. Collects issues #1-6 of Catwoman’s new series.",
+        "thumb" => "https://imgs.search.brave.com/bf_FlGnymAgmvweY8YiLAcyy600JdSMl_jHpdR3t5ro/rs:fit:612:612:1/g:ce/aHR0cHM6Ly9pNS53/YWxtYXJ0aW1hZ2Vz/LmNvbS9hc3IvNTA5/M2IxOTEtZTkzZS00/NTJjLThhYTQtYTEw/ZDY0ZTcxN2E5LjA5/N2UwZWM5NDgzYzky/OGExMjY4Nzk1Nzk2/NTliZWNjLmpwZWc_/b2RuV2lkdGg9NjEy/Jm9kbkhlaWdodD02/MTImb2RuQmc9ZmZm/ZmZm",
+        "price" => "$16.99",
+        "series" => "Catwoman",
+        "sale_date" => "2019-04-10",
+        "type" => "graphic novel",
+        "artists" => [
+            "Fernando Blanco",
+            "Joëlle Jones"
+        ],
+        "writers" => [
+            "Joëlle Jones"
+        ],
+    ],
+];
