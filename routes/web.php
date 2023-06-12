7a9a0e30719f18f0ac322df79cdf411b9d4b7c4a
raffaele-catalano <raf.catalano@gmail.com>
2023-06-12 16:11:04 +0200

    fixed web.php and added standard (not --resource) Controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\ComicController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,8 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::resource('home', PageController::class);
 
-Route::resource('comic', PageController::class);
+Route::resource('comic', ComicController::class);
