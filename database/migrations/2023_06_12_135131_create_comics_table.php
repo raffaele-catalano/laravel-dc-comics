f44b2da61b749fe4316c713771695a02196c3de4
raffaele-catalano <raf.catalano@gmail.com>
2023-06-12 16:07:30 +0200

    created a migration

---
+++ database/migrations/2023_06_12_135131_create_comics_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->text('thumb');
+            $table->string('price');
+            $table->string('series');
+            $table->date('sale_date');
+            $table->string('type');
+            $table->text('artists');
+            $table->text('writers');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+};
