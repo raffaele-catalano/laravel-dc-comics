fbb5b4742c7d89f9cbb061a455fe4054d3c600bd
raffaele-catalano <raf.catalano@gmail.com>
2023-06-12 18:41:14 +0200

    added 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.main')
+
+@section('content')
+    <div class="error-wrapper">
+        <div class="error-container text-center">
+            <img src="https://media1.giphy.com/media/hSvIJlprJ8Ji/giphy.gif?cid=ecf05e47sjhqlpgqiutsf3end69cjabp4g321imkd5ukq1n9&ep=v1_gifs_search&rid=giphy.gif&ct=g" alt="404">
+            <h3 class="fs-1"><span class="text-danger">Error 404</span> | <span class="text-primary">Comic not found</span></h3>
+        </div>
+    </div>
+@endsection
