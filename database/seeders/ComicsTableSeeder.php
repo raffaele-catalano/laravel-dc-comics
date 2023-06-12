e17324e6c8377e03bc666c3c25dde3da726623b0
raffaele-catalano <raf.catalano@gmail.com>
2023-06-12 16:32:02 +0200

    populated Comics table

--- database/seeders/ComicsTableSeeder.php
+++ database/seeders/ComicsTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Comic;
 
 class ComicsTableSeeder extends Seeder
 {
@@ -14,6 +15,19 @@ class ComicsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $comics = config('comics');
+        foreach($comics as $comic) {
+            $new_comic = new Comic();
+            $new_comic->title       = $comic['title'];
+            $new_comic->description = $comic['description'];
+            $new_comic->thumb       = $comic['thumb'];
+            $new_comic->price       = $comic['price'];
+            $new_comic->series      = $comic['series'];
+            $new_comic->sale_date   = $comic['sale_date'];
+            $new_comic->type        = $comic['type'];
+            $new_comic->artists     = implode(' - ', $comic['artists']);
+            $new_comic->writers     = implode(' - ', $comic['writers']);
+            $new_comic->save();
+        }
     }
 }
