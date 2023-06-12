f5fd44981793396fba74f7b324b8fc17becc5641
raffaele-catalano <raf.catalano@gmail.com>
2023-06-12 15:08:36 +0200

    setup of basic output page's layout

---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>DC-Comics</title>
+
+    {{-- Favicon --}}
+    <link rel="shortcut icon" href="/dc_favicon.ico" type="ico">
+
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    @vite('resources/js/app.js')
+
+</head>
+
+<body class="bg-dark">
+
+    <main class="text-light">
+        <div class="main-wrapper">
+            @yield('content')
+        </div>
+    </main>
+
+</body>
+
+</html>
