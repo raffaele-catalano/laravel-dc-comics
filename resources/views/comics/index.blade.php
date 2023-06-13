d1fe0b7149eae6d997af7b519a8665197ac2fdb7
raffaele-catalano <raf.catalano@gmail.com>
2023-06-13 13:48:24 +0200

    added "create" button in index.comics

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -2,7 +2,12 @@
 
 @section('content')
     <div class="container">
-        <h2 class="my-2 text-center text-primary fw-bold">Comics List</h2>
+        <div class="title-container d-flex justify-content-center">
+            <h2 class="m-3 text-center text-primary fw-bold align-middle">Comics List</h2>
+            <div class="create-container d-flex align-items-center">
+                <a href="#" class="btn btn-success fw-bold"><i class="fa-solid fa-plus fa-lg"></i></a>
+            </div>
+        </div>
         <table class="table table-dark">
             <thead>
                 <tr>
@@ -19,7 +24,7 @@
                         <td>{{ $comic->title }}</td>
                         <td>{{ $comic->series }}</td>
                         <td>
-                            <a href="{{ route('comics.show', $comic) }}" class="btn btn-primary fw-bold"><i class="fa-brands fa-golang fa-lg"></i></a>
+                            <a href="{{ route('comics.show', $comic) }}" class="btn btn-primary fw-bold"><i class="fa-solid fa-eye"></i></a>
                             <a href="#" class="btn btn-warning fw-bold"><i class="fa-solid fa-pencil"></i></a>
                             <a href="#" class="btn btn-danger fw-bold"><i class="fa-solid fa-trash-can"></i></a>
                         </td>
