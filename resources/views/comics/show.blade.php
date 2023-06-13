4d4eafbba744122071b540cf88d3c291aa5fb5ca
raffaele-catalano <raf.catalano@gmail.com>
2023-06-13 14:54:57 +0200

    added "create" button in show.comics

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,7 +1,12 @@
 @extends('layout.main')
 
 @section('content')
-<h2 class="text-center fw-bold text-warning my-3">{{ $comic->title }}</h2>
+        <div class="title-container d-flex justify-content-center">
+            <h2 class="my-1 text-center text-warning fw-bold align-middle">{{ $comic->title }}</h2>
+        </div>
+        <div class="edit-container d-flex align-items-center my-2">
+            <a href="#" class="btn btn-info fw-bold"><i class="fa-solid fa-pencil fa-lg"></i></a>
+        </div>
     <div class="row row-cols-2 w-75 d-flex justify-content-center align-items-center">
         <div class="col d-flex align-items-end flex-column">
             <img src="{{ $comic->thumb }}" class="w-50 mb-2" alt="comic-thumb">
