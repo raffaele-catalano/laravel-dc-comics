183b0a43e267cc1c40b86f0555e694fc6ec2f606
raffaele-catalano <raf.catalano@gmail.com>
2023-06-13 14:41:54 +0200

    added protected fillable

--- app/Models/Comic.php
+++ app/Models/Comic.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Comic extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'thumb',
+        'price',
+        'series',
+        'sale_date',
+        'type',
+        'artists',
+        'writers'
+    ];
 }
